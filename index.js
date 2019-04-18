09225f689972f6d486ceaf315f69593bf170c307
swapan-sixt <sampath.kumar@sixt.com>
2019-04-18 23:47:38 +0530

    Day 4

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.get('/invokeChat', function(request, resp) {
     var integrationScript = undefined;
     console.log('myUUID: ',myUUID);
 
-    var options = {
+    /*var options = {
         "method": "POST",
         "hostname": "api.chatbot.com",
         "port": null,
@@ -133,104 +133,15 @@ app.get('/invokeChat', function(request, resp) {
             "content-type": "application/json",
             "authorization": "Bearer "+DEVELOPER_ACCESS_TOKEN
         }
-    };
+    };*/
 
 
     
     return fkClient.doKeywordSearch(request.query["q"],10).then(function(value){
         console.log('shpng response.... ', value);
-        //var productTitle = JSON.parse(value.body).products[0].productBaseInfoV1.title;
-       console.log('options... ', options);
-        var req = http.request(options, function (res) {
-        var chunks = [];
-        console.log('htt request made... ', http);
-        res.on("data", function (chunk) {
-            console.log('data: ', chunk);
-            chunks.push(chunk);
-        });
-
-        console.log('reuqest made to:', options);
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            console.log('resp body: ', body.toString());
-            storyId = JSON.parse(body.toString()).id;
-            console.log('storyId: ', storyId);
-
-            //Create chat integration
-            var req1 = http.request(optionsIntegration, function (res1) {
-            var chunks1 = [];
-
-                res1.on("data", function (chunk1) {
-                    chunks1.push(chunk1);
-                });
-
-                res1.on("end", function () {
-                    var body1 = Buffer.concat(chunks1);
-                    console.log('resp body1: ', body1.toString());
-                    integrationId = JSON.parse(body1.toString()).id;
-                    console.log('integrationId: ', integrationId);
-
-                    //Update integration
-
-                    var optionsUpdateIntegration = {
-                        "method": "PUT",
-                        "hostname": "api.chatbot.com",
-                        "port": null,
-                        "path": "/integrations/widget/"+integrationId,
-                        "headers": {
-                            "content-type": "application/json",
-                            "authorization": "Bearer "+DEVELOPER_ACCESS_TOKEN
-                        }
-                    };
-
-                    var req2 = http.request(optionsUpdateIntegration, function (res2) {
-                    var chunks2 = [];
-
-                        res2.on("data", function (chunk2) {
-                            chunks2.push(chunk2);
-                        });
-
-                        res2.on("end", function () {
-                            var body2 = Buffer.concat(chunks2);
-                            console.log('resp body2: ', body2.toString());
-                            integrationScript = 'window.__be=window.__be||{},window.__be.id="'+integrationId+'",function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"==document.location.protocol?"https://":"http://")+"cdn.chatbot.com/widget/plugin.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();';
-                           
-                            //Update integration
-                            let output = {
-                                statusCode: 200,
-                                body: integrationScript,
-                            };
-
-                            console.log('--output--', output);
-                            resp.send(output);
-                            //Create chat integration
-                            
-
-                        });
-                    });
-
-                    req2.write("{\"welcomeScreen\":false,\"whiteLabel\":false,\"colors\":{\"background\":\"#21262e\",\"text\":\"#ffffff\",\"theme\":\"#1F8CEB\",\"themeText\":\"#fafafa\",\"responseBackground\":\"#e9eef4\",\"responseText\":\"#646464\"},\"company\":{\"name\":\"Chirrp\",\"description\":\"Share your needs in under 30 seconds to research '"+request.query["q"]+"'\",\"avatar\":{\"enabled\":false,\"url\":\"\"},\"background\":{\"enabled\":false,\"url\":\"\"},\"social\":{\"facebook\":\"\",\"twitter\":\"\",\"linkedin\":\"\"}},\"chatWindow\":{\"theme\":\"default\"},\"chatButton\":{\"theme\":\"bubble\",\"text\":\"Chat with us!\"},\"storyId\":"+storyId+"}");
-                    req2.end();
-
-                    //callback(undefined, "success");
-
-                });
-            });
-
-            req1.write("{\"storyId\":\""+storyId+"\",\"company\":{\"name\":\"chirrp\"}}");
-            req1.end();
-
-        });
-    });
-
-    req.write("{\"name\":\""+myUUID+"\",\"description\":\"Share your needs in under 30 seconds to research '"+request.query["q"]+"'\"}");
-    req.end();
-            
-        
-            
-       
-        
+        var productTitle = JSON.parse(value.body).products[0].productBaseInfoV1.title;
+        console.log('options... ', options);
+        resp.send(productTitle);
     });
             
 });
