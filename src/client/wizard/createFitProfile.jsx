4e212bd364dda3a6f17c895504e7d5e49feb376f
swapan-sixt <sampath.kumar@sixt.com>
2019-06-20 19:23:17 +0530

    Day 8

--- src/client/wizard/createFitProfile.jsx
+++ src/client/wizard/createFitProfile.jsx
@@ -8,6 +8,87 @@ import Shop from './shop.jsx';
 import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+let searchResults = [
+                      {
+                        "id": 1,
+                        "thumbnail": "th01.jpg",
+                        "title": "Sri Annapoorneshwari Catering",
+                        "address": "365, Ideal Homes Layout, RR Nagar",
+                        "specialities": [
+                          {
+                            "icon": "ic_food.jpg",
+                            "title": "Karnataka, Andhra, Tamil, North Indian"
+                          },
+                          {
+                            "icon": "ic_group.jpg",
+                            "title": "Ideal for parties of all sizes"
+                          }
+                        ],
+                        "verifiedStories": [
+                          {
+                            "avatar": "",
+                            "title": "Good food served \uD83D\uDC4D",
+                            "sentiment": "positive",
+                            "story": [
+                              {
+                                "from": "bot",
+                                "message": ""
+                              },
+                              {
+                                "from": "user",
+                                "message": ""
+                              }
+                            ]
+                          },
+                          {
+                            "avatar": "",
+                            "title": "Timely service",
+                            "sentiment": "neutral"
+                          }
+                        ],
+                        "cta": "Get instant quote"
+                      },
+                      {
+                          "id": 2,
+                          "thumbnail": "th01.jpg",
+                          "title": "Bangalore Catering",
+                          "address": "Ashoka pillar, 2nd Block Jayanagar",
+                          "specialities": [
+                            {
+                              "icon": "ic_food.jpg",
+                              "title": "North Indian, South Indian"
+                            },
+                            {
+                              "icon": "ic_group.jpg",
+                              "title": "Ideal for parties of all sizes"
+                            }
+                          ],
+                          "verifiedStories": [
+                            {
+                              "avatar": "",
+                              "title": "Good food served \uD83D\uDC4D",
+                              "sentiment": "positive",
+                              "story": [
+                                {
+                                  "from": "bot",
+                                  "message": ""
+                                },
+                                {
+                                  "from": "user",
+                                  "message": ""
+                                }
+                              ]
+                            },
+                            {
+                              "avatar": "",
+                              "title": "Timely service",
+                              "sentiment": "neutral"
+                            }
+                          ],
+                          "cta": "Get instant quote"
+                        }
+                    ];
+
 class ResultHeadline extends Component {
     render() {
         return (
@@ -20,18 +101,52 @@ class ResultHeadline extends Component {
 }
 
 class Card extends Component {
+    renderSpecialities(specialities) {
+        let results = [];
+        specialities.map((item,key)=> {
+                            results.push(<div className="icon-c"><img src={`./img/${item.icon}`}/><div className="icon-title">{item.title}</div></div>);
+                        });
+        return results;
+    }
     render() {
         return (
-            <div>Card</div>
+            <div className="card">
+               <div className="card-c">
+                    <div className="detail">
+                        <div className="thumbnail-section">
+                            <img src={`./img/${this.props.thumbnail}`} />
+                        </div>
+                        <div className="detail-section">
+                            <div className="title">{this.props.title}</div>
+                            <div className="address">{this.props.address}</div>
+                            <div className="spec-c">{this.renderSpecialities(this.props.specialities)}</div>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="detail story">
+                    </div>
+               </div>
+               <div className="card-d"></div>
+            </div>
         );
     }
 }
 
 class SearchResult extends Component {
+
+    renderCards() {
+        let results = [];
+        searchResults.map((item,key)=> {
+            results.push(<Card title={item.title} address={item.address} specialities={item.specialities} thumbnail={item.thumbnail}/>);
+        });
+        return results;
+    }
+
     render() {
         return (
             <div>
                 <ResultHeadline title="Services customers used & loved" />
+                {this.renderCards()}
             </div>
         );
     }
