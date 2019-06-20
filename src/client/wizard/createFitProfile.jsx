0fc0233d465302c9936282024845e099a39d49e6
swapan-sixt <sampath.kumar@sixt.com>
2019-06-20 17:25:56 +0530

    Day 8

--- src/client/wizard/createFitProfile.jsx
+++ src/client/wizard/createFitProfile.jsx
@@ -15,81 +15,12 @@ class CreateFitProfile extends Component {
       history: PropTypes.object.isRequired
     }
     componentDidMount(){
-      gtag('event', 'Stage', {'event_category':'load', 'event_label':'fit-profile'}); 
-      gtag('config', 'UA-107877274-1',{'page_title': 'fit_profile','page_location': 'http://www.poseding.com/search','page_path': '/search'});
-      Loggr.Log.trackUser(uid, "", "Fit profile page visited.");
-      console.log('this.props.history:',this.props.history);
-      if(variation == 1){
-        $('#captureButton').show();
-        $('.selfie').show();
-        $('#captureButton').show();
-      } else if(variation == 0) {
-        // $('#selfieMsg').html("Take a selfie when you're ready!");
-        // $('.loading-container').show();
-      }
 
-      if(!chromeOnly) {
-        $('#capture-photo').hide();
-      }
-      var h = this.props.history;
-      const unlisten = h.listen((location, action) => {
-        // location is an object like window.location
-        console.log('history.listen:', location);
-        console.log('nonLatestChrome: ', chromeOnly);
-        console.log('location.pathname: ', location.pathname);
-        if(chromeOnly && location.pathname == '/search') {
-          console.log('reloading');
-          window.location.reload();
-        }
-      })
-      window.onScanComplete = function() {
-        var fitStr = 'faceH:'+localStorage.getItem('faceH')+'faceW:'+localStorage.getItem('faceW')+'faceX:'+localStorage.getItem('faceX')+'faceY:'+localStorage.getItem('faceY')+'shoulderH:'+localStorage.getItem('shoulderH')+'shoulderW:'+localStorage.getItem('shoulderW')+'shoulderX:'+localStorage.getItem('shoulderX')+'shoulderY:'+localStorage.getItem('shoulderY');
-        // alert(fitStr);
-        h.push('/search/update');
-        Loggr.Log.trackUser(uid, "", "selfie captured.");
-        setTimeout("sendPic()",1500);
-        //if(variation == 0) {
-          $('.outline').hide();
-        //}
-        $('#capture-photo').hide();
-        $('#snap').hide();
-        $('#camera-stream').hide();
-      }
     }
     render(){
         const { match, location, history } = this.props;
         return (<div>
-                <div className="content" style={{width: '90%'}}>
-                  <div class="select">
-                  </div>
-                  <div className="fit-profile-heading">Help us identify your body shape and skin tone.</div>
-                  <div className="selfie-tbl">
-                    <div className="selfie-img-cell">
-                      <img className="selfie" src="../img/selfieicon.png" style={{width:'70px',marginTop:'-10px'}}></img>
-                    </div>
-                    <div style={{display:'table-cell', marginTop: '40px'}} id="captureButton">
-                      <label className="btn" htmlFor="file-input" style={{width:'226px',top:'30px'}}><span style={{left: '16px'}}><i className="material-icons" style={{left: '-32px',position:'absolute',top:'-2px'}}>camera_alt</i>Upload Selfie</span></label>
-                    </div>
-                  </div>
-                  <div className="dot-line"><div className="line-left"></div><div className="line-right"></div><span>or</span></div>
-                  <div className="selfie-tbl">
-                    <div className="selfie-img-cell">
-                      <img className="selfie" src="../img/quizicon.jpg" style={{width:'60px',marginTop:'-10px'}}></img>
-                    </div>
-                    <div style={{display:'table-cell'}}>
-                      <Link to="/search/update" className="btn upload-btn" style={{width:'226px',top:'30px',left:'6px'}}><span>Take style quiz</span></Link>
-                    </div>
-                  </div>
-                </div> 
-                <div className="loading-container" style={{display:'none'}}><div className="loading"></div><span className="loading-msg">Loading camera. Please wait...</span></div>
-                 <input type="file" style={{display:'none'}} accept="image/*" name="file-1[]" id="file-input" dcvalue="" className="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple="" dcvalue=""></input>
-                 <center><label for="file-input"></label>
-                 <div id="selfieImage"><img id="selfieimg"></img></div>
-                 <div id="result" className="result">
-                 <a id="selfie-url" target="_blank"><canvas id="load-img-canvas"></canvas></a>
-                 </div></center>
-                 
-                 
+                loader
               </div>
           );
     }
@@ -103,7 +34,7 @@ render(<Router>
         <div>
         <Route path="/search" render={()=>(
             <div className="results">
-            <Route exact path="/search" />
+            <Route exact path="/search" component={CreateFitProfile}/>
             <Route exact path="/search/update" component={CaptureMeasurements}/>
             <Route exact path="/search/quiz" component={QuizWithRouter}/>
           </div>)} />
